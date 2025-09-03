e1ab6dfaacd962812ef47b368aeb09f367861460
VigilantesMasked <davidkgonzalez4177@gmail.com>
2025-09-03 15:34:29 +0000

    wrote code to calulate bmi

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
- 
+    int height = 61;
+ int weight= 130;
+//Convert pounds to kilograms:
+double kg = weight*0.453592;
+//Convert inches to meters:
+double m=height*0.0254;
+//compute bmi
+double bmi = kg / (m*m);
 
-
+cout<< "The bmi for this person is : " << bmi << "."<< endl;
 }
